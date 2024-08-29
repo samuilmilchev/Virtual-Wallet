1e7e17e457b98b66b7888eb0838004a1ca27d431
Alexander Georgiev <alexander.nikolaev.georgiev@gmail.com>
2024-08-29 22:53:53 +0300

    ConvertFundsViewModel

---
+++ Virtual Wallet/Models/ViewModels/ConvertFundsViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Virtual_Wallet.Models.Entities;
+
+namespace Virtual_Wallet.Models.ViewModels
+{
+    public class ConvertFundsViewModel
+    {
+        //decimal amount, Currency fromCurrency, Currency toCurrency, Wallet fromWallet, Wallet toWallet, User user
+        public Wallet FromWallet { get; set; }
+        public Wallet ToWallet { get; set; }
+        public User User { get; set; }
+        public Currency FromCurrency { get; set; }
+        public Currency ToCurrency { get; set; }
+        public decimal Amount { get; set; }
+    }
+}
